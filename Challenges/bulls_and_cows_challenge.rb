82e042f4733aea0b6798e264b441de38bb3f7ac7
mardiniii <sezama03@hotmail.com>
2016-10-01 10:47:13 -0500

    Add bulls and cows challenge

---
+++ Challenges/bulls_and_cows_challenge.rb
@@ -0,0 +1,9 @@
+# Picas y Fijas o Bulls and Cows
+
+# Es un juego con números que data de un siglo o más. Es jugado por dos adversarios. El juego también se puede jugar con tres dígitos en lugar de cuatro.
+
+# En una hoja de papel, cada uno de los jugadores escribe un número secreto de 4 dígitos. Las cifras deben ser todas diferentes. Luego, a su vez, los jugadores tratan de adivinar el número de su oponente, que da el número de aciertos. Si las cifras son coincidentes en sus posiciones correctas, son "fijas"(o bulls), si en diferentes posiciones, son "picas"(o cows). Ejemplo:
+
+# El número secreto: 4271
+# Intento del oponente: 1234
+# Respuesta: una fija y dos picas. (La fija es "2", las picas son "4" y "1".)
