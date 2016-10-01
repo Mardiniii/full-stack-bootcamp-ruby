547061fd6e4751e01f06248f188c953410fdeab9
mardiniii <sezama03@hotmail.com>
2016-10-01 14:45:44 -0500

    Add hash from txt file example

---
+++ Ruby/hash_from_file.rb
@@ -0,0 +1,20 @@
+# Create an array of hashes with the information from a txt file (makers.txt)
+
+filename = 'makers.txt'
+txt = open(filename)
+
+students = []
+
+txt.each_line do |line|
+  student = line.split(',')
+  name = student[0]
+  age = student[1].to_i
+  profession = student[2]
+  height = student[3].to_f
+
+  student_hash = { name: name, age: age, profession: profession, height: height }
+
+  students.push(student_hash)
+end
+
+puts students
