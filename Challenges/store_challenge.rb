b6335d3785e5ed20e428878abb90d45db4bb3981
mardiniii <sezama03@hotmail.com>
2016-10-01 13:46:24 -0500

    Add store_challenge solution for part_1

--- Challenges/store_challenge.rb
+++ Challenges/store_challenge.rb
@@ -15,3 +15,134 @@
 # Al momento de comenzar el programa deben haber por lo menos 3 productos en el store
 # Una vez ejecute una opción debo volver al menú
 # Debe existir una opción para salir del programa
+
+# Print all the products in the stock
+def print_product(product, index)
+  puts """Product #{index +1}
+    Name: #{product[:name]}
+    Brand: #{product[:brand]}
+    Price: #{product[:price]}
+    Quantity: #{product[:quantity]}
+    Reference: #{product[:reference]}
+  """
+end
+
+def show_total_stock(products)
+  products.each_with_index do |product, index|
+    print_product(product, index)
+  end
+end
+
+# Sum all the product prices and return its value
+def sum_all_the_prices(products)
+  sum = 0
+  products.each do |product|
+    sum = sum + ( product[:price] * product[:quantity])
+  end
+  sum
+end
+
+# Sum all the product prices and return its value
+def sum_all_the_articles(products)
+  sum = 0
+  products.each do |product|
+    sum = sum + product[:quantity]
+  end
+  sum
+end
+
+# Search a product with the reference's argument. If it exist returns product's index if not false
+def search_product(products, reference)
+  product_position = false
+  products.each_with_index do |product, index|
+    if product[:reference] == reference
+      product_position = index
+    end
+  end
+  product_position
+end
+
+# Add a new product
+def add_product(products, new_product)
+  products.push(new_product)
+end
+
+def breakline
+  puts "\n"
+end
+
+def prompt
+  print '> '
+end
+
+product_1 = { name: 'T - Shirt', brand: 'Quicksilver', price: 30_000, quantity: 10, reference: 'REF001' }
+product_2 = { name: 'Jean', brand: 'Levis', price: 200_000, quantity: 20, reference: 'REF002' }
+product_3 = { name: 'Jacket', brand: 'Custer', price: 100_000, quantity: 30, reference: 'REF003' }
+
+products = [ product_1, product_2, product_3 ]
+
+option = 0
+while option != 6
+  puts """
+    Bienvenido a Make It Real Store
+
+    1. Total stock
+    2. Show product
+    3. Get inventory amount $$$
+    4. Average price
+    5. Add product
+    6. Exit
+  """
+  prompt
+  option = gets.chomp.to_i
+  case option
+  when 1
+    # Call show total stock function
+    show_total_stock(products)
+  when 2
+    puts 'Enter product reference'
+    reference = gets.chomp
+    # Call search product function
+    product_position = search_product(products, reference)
+    if product_position.is_a? Fixnum
+      # Call print product
+      print_product(products[product_position], product_position)
+    else
+      breakline
+      puts "Product not found!"
+    end
+  when 3
+    # Call sum all the prices function
+    sum_prices = sum_all_the_prices(products)
+    breakline
+    puts "The sum of the prices is: #{sum_prices}"
+  when 4
+    # Call sum all the prices function
+    sum_prices = sum_all_the_prices(products)
+    sum_articles = sum_all_the_articles(products)
+    breakline
+    puts "The average price is: #{sum_prices / sum_articles}"
+  when 5
+    # Get product info from the user
+    puts "Name: "
+    name = gets.chomp
+    puts 'Brand: '
+    brand = gets.chomp
+    puts 'Price: '
+    price = gets.chomp.to_i
+    puts 'Quantity: '
+    quantity = gets.chomp.to_i
+    puts 'Reference: '
+    reference = gets.chomp
+
+    # Create
+    new_product = { name: name, brand: brand, price: price, quantity: quantity, reference: reference }
+    add_product(products, new_product)
+    print_product(products.last, products.size - 1)
+    puts 'The product has been created succesfully'
+  when 6
+    puts "Bye bye user!"
+  else
+    puts "Por favor escoja una opción correcta"
+  end
+end
